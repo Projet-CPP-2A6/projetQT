44427304784c45953105c76e555db54bc03820a0
Salemdiber <salem.diber@esprit.tn>
2024-03-14 21:16:33 +0100

    commit ( crud avec metiers non avancés )

---
+++ eventmodel.h
@@ -0,0 +1,15 @@
+#ifndef EVENTMODEL_H
+#define EVENTMODEL_H
+#include <QSqlQueryModel>
+#include <QPixmap>
+
+class eventmodel : public QSqlQueryModel
+        {
+        public:
+        public:
+            void refresh();
+            void refresh1();
+        };
+
+
+#endif // EVENTMODEL_H
